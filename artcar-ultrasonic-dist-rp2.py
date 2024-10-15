31aed8bb3475fd241151fcf19a4a9ef93f25ec9f
bradcar <bc@bradcarlile.com>
2024-10-15 06:12:45 -0700

    Update artcar-ultrasonic-dist-rp2.py
    added startup code section

--- artcar-ultrasonic-dist-rp2.py
+++ artcar-ultrasonic-dist-rp2.py
@@ -22,10 +22,17 @@ loopTime = time.ticks_ms()
 soundSpeed = 344.11
 debug = True
 
+# startup code
+print("Starting...")
+print(f"Speed Sound: {soundSpeed:.1f} m/s")
+
+
+# main loop
 while True:
     elapsed_time = time.ticks_diff(time.ticks_ms(), loopTime)
     print("Elapsed time:", elapsed_time, "msec\n")
     loopTime = time.ticks_ms()
+    
     #update speed of sound using temp & humidity from dht22
     try:
         dht_sensor.measure()
@@ -35,7 +42,7 @@ while True:
         # Speed sound with temp, best I could find was: (20.05 * sqrt(273.16 + tempC))
         # Speed sound temp & humidity calculator: http://resource.npl.co.uk/acoustics/techguides/speedair/
         # I did a 2d linear fit with formula above to add correction, error is +/-0.07% in range
-        # valid over the temp range 0 to 30C temp (273.15 - 303.15 K) and for the pressure range 75 - 102 kPa
+        # valid over the temp range 0 to 30C temp & 75 to 102 kPa pressure
         soundSpeed = (20.05 * math.sqrt(273.16 + tempC)) + (0.0006545 * humidity + 0.00475) * tempC + (0.001057 * humidity + 0.07121)
         #
         if debug:
