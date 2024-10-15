a04562160c0f6d8a07c9719998ed626f010660ed
bradcar <bc@bradcarlile.com>
2024-10-15 05:26:34 -0700

    Update artcar-ultrasonic-dist-rp2.py
    added dht22 sensor

--- artcar-ultrasonic-dist-rp2.py
+++ artcar-ultrasonic-dist-rp2.py
@@ -1,8 +1,29 @@
 from machine import Pin, Timer
+import machine
+import dht
+import time
+
+dht_pin = machine.Pin(2)
 led = Pin(25, Pin.OUT)
-tim = Timer()
+
+timecall = Timer()
+
+
 def tick(timer):
     global led
     led.toggle()
 
-tim.init(freq=2.5, mode=Timer.PERIODIC, callback=tick)
+
+timecall.init(freq=2.5, mode=Timer.PERIODIC, callback=tick)
+dht_sensor = dht.DHT22(dht_pin)
+
+while True:
+    try:
+        dht_sensor.measure()
+        tempC = dht_sensor.temperature()
+        humidity = dht_sensor.humidity()
+        print(f"Temp °C : {tempC:.2f}")
+        print(f"Humidity: {humidity:.2f}%")
+    except Exception as e:
+        print("Error reading DHT22:", str(e))
+    time.sleep(0.1)
