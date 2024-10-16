ef8f9894703d9bb0ac405dbaf8ccd79abd6a192f
bradcar <bc@bradcarlile.com>
2024-10-16 06:41:28 -0700

    Update artcar-ultrasonic-dist-rp2.py
    cleaned up functions and code throughout, too many changes

--- artcar-ultrasonic-dist-rp2.py
+++ artcar-ultrasonic-dist-rp2.py
@@ -48,40 +48,95 @@ device = ssd1309(serial)
 i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
 oled = SSD1306_I2C(128, 64, i2c)
 
+dht_sensor = dht.DHT22(dht_pin)
 
-timecall = Timer()
+#image2cpp (convert png into C code): https://javl.github.io/image2cpp/
+# then replace ", 0"  with "\"
+# const unsigned char bitmap_artcar_image[] PROGMEM = {0xc9, 0x04, 0x59, ...
+# goes to bitmap_artcar_image=bytearray(b'\xc9\x04\x59
+
+# 'art-car-imag', 56x15px
+#bitmap_artcar_image=bytearray(b'\xc9\x04\x59\x11\x0c\x08\x43\xc8\x82\x8e\x90\x93\x10\x93\xe0\x63\x00\x78\xe0\xe3\x07\xff\x9f\xff\xff\xff\xfc\xff\xc0\x00\x00\x00\x00\x00\x03\x40\x1c\xf3\xe3\x8e\x78\x02\x47\x22\x88\x84\x51\x44\xe2\x45\x22\x88\x84\x11\x44\xa2\x47\x22\xf0\x84\x11\x78\xe2\x20\x3e\x88\x84\x1f\x44\x04\x10\x22\x88\x84\x51\x44\x08\x0c\x22\x88\x83\x91\x44\x30\x03\x00\x00\x00\x00\x00\xc0\x00\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00')
+bitmap_artcar_image = bytearray([
+  0xc9, 0x04, 0x59, 0x11, 0x0c, 0x08, 0x43, 0xc8, 0x82, 0x8e, 0x90, 0x93, 0x10, 0x93, 0xe0, 0x63,
+  0x00, 0x78, 0xe0, 0xe3, 0x07, 0xff, 0x9f, 0xff, 0xff, 0xff, 0xfc, 0xff, 0xc0, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x03, 0x40, 0x1c, 0xf3, 0xe3, 0x8e, 0x78, 0x02, 0x47, 0x22, 0x88, 0x84, 0x51, 0x44,
+  0xe2, 0x45, 0x22, 0x88, 0x84, 0x11, 0x44, 0xa2, 0x47, 0x22, 0xf0, 0x84, 0x11, 0x78, 0xe2, 0x20,
+  0x3e, 0x88, 0x84, 0x1f, 0x44, 0x04, 0x10, 0x22, 0x88, 0x84, 0x51, 0x44, 0x08, 0x0c, 0x22, 0x88,
+  0x83, 0x91, 0x44, 0x30, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0x00, 0xff, 0xff, 0xff, 0xff,
+  0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+])
+
+loopTime = time.ticks_ms()
+# init soundSpeed if dht22 sensor fails
+soundSpeed = 344.11
+debug = False
 
 
 def blink(timer):
+    """
+    Callback function to toggle the LED state, creating a blinking effect.
+    To enable background blinking call with:
+    timecall.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
+    
+    Args:
+        timer: The Timer object triggering the callback.        
+    """
     global led
     led.toggle()
     
 
-
+# if want blinking happening in background
+#timecall = Timer()
 #timecall.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
-dht_sensor = dht.DHT22(dht_pin)
-loopTime = time.ticks_ms()
-# init soundSpeed if dht22 sensor fails
-soundSpeed = 344.11
-debug = True
 
-def ultra(speed):
-    # SUPER SIMPLE, write one with time outs
-    # tried with 3.3v, but really need 5v for ultrasonic & display
-    # https://forums.raspberrypi.com/viewtopic.php?f=37&t=18291&start=25
-    # https://forums.raspberrypi.com/viewtopic.php?t=287381
-    # https://forums.raspberrypi.com/viewtopic.php?t=287381
+    
+def ultra(speed, timeout=50000):  # timeout in microseconds
+    """
+    Measure the distance using an ultrasonic sensor with a timeout.
+     * tried with 3.3v, but really need 5v for ultrasonic & display
+     * todo determine if this should be 30000, like in Arduino code
+     * todo create consistent error handling throughout code
+
+    
+    Args:
+        speed (float): Speed of sound in m/s.
+        timeout (int): Maximum microseconds to wait for the echo signal return
+        
+    Returns:
+        float: cm distance or
+               -1 if the measurement times out waiting for each to go high
+               -2 if the measurement times out waiting for each to go low
+
+    """
     trigger.low()
     utime.sleep_us(2)
     trigger.high()
     utime.sleep_us(5)
     trigger.low()
+
+    # Wait for echo to go high, with timeout
+    start = utime.ticks_us()
     while echo.value() == 0:
-        signaloff = utime.ticks_us()
+        if utime.ticks_diff(utime.ticks_us(), start) > timeout:
+            #print("Ultrasonic Timeout waiting for echo to go high: too close or malfunction.")
+            return -1  # Return -1 to indicate a timeout
+
+    signaloff = utime.ticks_us()
+
+    # Wait for echo to go low, with timeout
+    start = utime.ticks_us()
     while echo.value() == 1:
-        signalon = utime.ticks_us()
-    distance = ((signalon - signaloff) * speed/20000.0)
-    return (distance)
+        if utime.ticks_diff(utime.ticks_us(), start) > timeout:
+            #print("Ultrasonic Timeout waiting for echo to go low: too far no signal returned.")
+            return -2  # Return -1 to indicate a timeout
+
+    signalon = utime.ticks_us()
+
+    # Calculate cm distance
+    distance = ((signalon - signaloff) * speed / 20000.0)
+    return distance
+
 
 # projects: https://www.tomshardware.com/news/raspberry-pi
 # fonts: https://www.youtube.com/watch?v=kxlN1knBpQ0
@@ -90,14 +145,12 @@ def ultra(speed):
 
 # startup code
 print("Starting...")
-print(f"Speed Sound: {soundSpeed:.1f} m/s\n")
+print(f"Default Speed Sound: {soundSpeed:.1f} m/s\n")
 
-bitmap_artcar_image=bytearray(b'\xc9\x04\x59\x11\x0c\x08\x43\xc8\x82\x8e\x90\x93\x10\x93\xe0\x63\x00\x78\xe0\xe3\x07\xff\x9f\xff\xff\xff\xfc\xff\xc0\x00\x00\x00\x00\x00\x03\x40\x1c\xf3\xe3\x8e\x78\x02\x47\x22\x88\x84\x51\x44\xe2\x45\x22\x88\x84\x11\x44\xa2\x47\x22\xf0\x84\x11\x78\xe2\x20\x3e\x88\x84\x1f\x44\x04\x10\x22\x88\x84\x51\x44\x08\x0c\x22\x88\x83\x91\x44\x30\x03\x00\x00\x00\x00\x00\xc0\x00\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00')
 # test print at top of oled
 fb = framebuf.FrameBuffer(bitmap_artcar_image,56,15, framebuf.MONO_HLSB)
 oled.blit(fb,40,8)
 oled.show()
-
 
 '''
 with canvas(device) as draw:
@@ -107,9 +160,15 @@ with canvas(device) as draw:
 
 # main loop
 while True:    
-    dist = ultra(soundSpeed)
-
-    #if need to flip display
+    dist = ultra(soundSpeed, timeout=30000)  # 30ms timeout 257cm
+    if dist == -1:
+        print("error Ultrasonic: too close or malfunction.")
+        dist = 0
+    elif dist == -2:
+        print("error Ultrasonic: no signal returned, too far.")
+        dist = 300
+    
+    #if need to flip display 
     #oled.rotate(180)
     #oled.invert(1)
 
@@ -125,35 +184,31 @@ while True:
             tempC = dht_sensor.temperature()
             tempF = (tempC * 9.0 / 5.0) + 32.0
             humidity = dht_sensor.humidity()
-            # Speed sound with temp, best I could find was: (20.05 * sqrt(273.16 + tempC))
+            
+            # Speed sound with temp correction: (20.05 * sqrt(273.16 + tempC))
             # Speed sound temp & humidity calculator: http://resource.npl.co.uk/acoustics/techguides/speedair/
-            # using online calculated constructed spreadsheet of values
-            # then 2d linear fit with formula above to add correction, error is +/-0.07% in range
-            # valid for 0C to 30C temp & 75 to 102 kPa pressure
+            # did a 2d linear fit of temp vs temp&humid speeds to create my own correction, error is now +/-0.07%            # valid for 0C to 30C temp & 75 to 102 kPa pressure
             soundSpeed = (20.05 * math.sqrt(273.16 + tempC)) + (0.0006545 * humidity + 0.00475) * tempC + (0.001057 * humidity + 0.07121)
-            #
+
             if debug:
                 #print(f"Temp °C : {tempC:.2f}")
                 print(f"Temp °F : {tempF:.2f}")
                 print(f"Humidity: {humidity:.2f}%")
                 print(f"Speed Sound: {soundSpeed:.1f} m/s")
                 print("Distance: ",dist,"cm\n")
-
-            # fill with black and the reprint
-            oled.fill(0)
-            oled.text(f"TempF= {tempF:.1f}F", 0, 0)
-            oled.text(f"Humid= {humidity:.1f}%", 0, 12)
-            oled.text(f"Sound={soundSpeed:.1f}m/s", 0, 24)
-            oled.text(f"Dist= {dist:.1f}cm", 0, 55)
-            # don't need to change PROGMEM DATA layout just remove: ", 0" replace with "\"
-            # const unsigned char bitmap_artcar_image[] PROGMEM = {0xc9, 0x04, 0x59, ...
-            # goes to bitmap_artcar_image=bytearray(b'\xc9\x04\x59
-            bitmap_artcar_image=bytearray(b'\xc9\x04\x59\x11\x0c\x08\x43\xc8\x82\x8e\x90\x93\x10\x93\xe0\x63\x00\x78\xe0\xe3\x07\xff\x9f\xff\xff\xff\xfc\xff\xc0\x00\x00\x00\x00\x00\x03\x40\x1c\xf3\xe3\x8e\x78\x02\x47\x22\x88\x84\x51\x44\xe2\x45\x22\x88\x84\x11\x44\xa2\x47\x22\xf0\x84\x11\x78\xe2\x20\x3e\x88\x84\x1f\x44\x04\x10\x22\x88\x84\x51\x44\x08\x0c\x22\x88\x83\x91\x44\x30\x03\x00\x00\x00\x00\x00\xc0\x00\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00')
-            fb = framebuf.FrameBuffer(bitmap_artcar_image,56,15, framebuf.MONO_HLSB)
-            oled.blit(fb,22,36)
-            oled.show()
         except Exception as e:
             print("Error reading DHT22:", str(e))
+        
+        # black dispay, then update results
+        oled.fill(0)
+        oled.text(f"TempF= {tempF:.1f}F", 0, 0)
+        oled.text(f"Humid= {humidity:.1f}%", 0, 12)
+        oled.text(f"Sound={soundSpeed:.1f}m/s", 0, 24)
+        oled.text(f"Dist= {dist:.1f}cm", 0, 55)
+        bitmap_artcar_image=bytearray(b'\xc9\x04\x59\x11\x0c\x08\x43\xc8\x82\x8e\x90\x93\x10\x93\xe0\x63\x00\x78\xe0\xe3\x07\xff\x9f\xff\xff\xff\xfc\xff\xc0\x00\x00\x00\x00\x00\x03\x40\x1c\xf3\xe3\x8e\x78\x02\x47\x22\x88\x84\x51\x44\xe2\x45\x22\x88\x84\x11\x44\xa2\x47\x22\xf0\x84\x11\x78\xe2\x20\x3e\x88\x84\x1f\x44\x04\x10\x22\x88\x84\x51\x44\x08\x0c\x22\x88\x83\x91\x44\x30\x03\x00\x00\x00\x00\x00\xc0\x00\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00')
+        fb = framebuf.FrameBuffer(bitmap_artcar_image,56,15, framebuf.MONO_HLSB)
+        oled.blit(fb,22,36)
+        oled.show()
     #
     #Every loop do this
     led.toggle()
