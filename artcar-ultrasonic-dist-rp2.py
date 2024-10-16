761b4eb7a4fe2fe81974b6f23107a2095b546bb7
bradcar <bc@bradcarlile.com>
2024-10-16 07:18:50 -0700

    Update artcar-ultrasonic-dist-rp2.py
    code structure cleanups

--- artcar-ultrasonic-dist-rp2.py
+++ artcar-ultrasonic-dist-rp2.py
@@ -20,36 +20,29 @@ from pillow import ImageFont
 #gc.collect() before executing gc.mem_free()
 
 # pins
+# https://www.tomshardware.com/how-to/oled-display-raspberry-pi-pico
+i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
 dht_pin = machine.Pin(2)
 trigger = Pin(3, Pin.OUT)
 echo = Pin(4, Pin.IN)
-
 led = Pin(25, Pin.OUT)
 
-# ic2 pins
-# https://www.tomshardware.com/how-to/oled-display-raspberry-pi-pico
+oled = SSD1306_I2C(128, 64, i2c)
+dht_sensor = dht.DHT22(dht_pin)
 
 # spi pins 8,9,10,11,25
-# vcc = 3.3v
-# gnd
 # scl (SCLK) GPIO 11 p15
 # SDA (MOSI) GPIO 10 p14
 # RES (RST)  ? RESET p30
 # DC GPIO 24? GPIO 9 p12
 # CS chip select CE chip enable GPIO 8 p11
-
 # hints on SPI interface here? https://coxxect.blogspot.com/2024/10/multi-ssd1306-oled-on-raspberry-pi-pico.html
-'''
 #display SSD spi
 #serial = spi(device=0, port=0, gpio_SCLK=15, gpio_SDA=14, gpio_RST=30, gpio_DC=12, gpio_CE=11)
-serial = spi(port=0, device=0, gpio_DC=12, gpio_RST=25, gpio_CS=11)
-device = ssd1309(serial)
-'''
-i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
-oled = SSD1306_I2C(128, 64, i2c)
+#serial = spi(port=0, device=0, gpio_DC=12, gpio_RST=25, gpio_CS=11)
+#device = ssd1309(serial)
 
-dht_sensor = dht.DHT22(dht_pin)
-
+#  DISPLAY IMAGES
 #image2cpp (convert png into C code): https://javl.github.io/image2cpp/
 # then replace ", 0"  with "\"
 # const unsigned char bitmap_artcar_image[] PROGMEM = {0xc9, 0x04, 0x59, ...
@@ -70,6 +63,10 @@ bitmap_artcar_image = bytearray([
 loopTime = time.ticks_ms()
 # init soundSpeed if dht22 sensor fails
 soundSpeed = 344.11
+tempC = 0.0
+tempF = 0.0
+humidity = 0.0
+errorstate = False
 debug = False
 
 
@@ -98,7 +95,6 @@ def ultra(speed, timeout=50000):  # timeout in microseconds
      * todo determine if this should be 30000, like in Arduino code
      * todo create consistent error handling throughout code
 
-    
     Args:
         speed (float): Speed of sound in m/s.
         timeout (int): Maximum microseconds to wait for the echo signal return
@@ -107,7 +103,6 @@ def ultra(speed, timeout=50000):  # timeout in microseconds
         float: cm distance or
                -1 if the measurement times out waiting for each to go high
                -2 if the measurement times out waiting for each to go low
-
     """
     trigger.low()
     utime.sleep_us(2)
@@ -147,10 +142,33 @@ def ultra(speed, timeout=50000):  # timeout in microseconds
 print("Starting...")
 print(f"Default Speed Sound: {soundSpeed:.1f} m/s\n")
 
-# test print at top of oled
+# at start print artcar image at top of oled, check DHT Sensor
+oled.fill(0)
 fb = framebuf.FrameBuffer(bitmap_artcar_image,56,15, framebuf.MONO_HLSB)
 oled.blit(fb,40,8)
 oled.show()
+try:
+    dht_sensor.measure()
+    tempC = dht_sensor.temperature()
+    tempF = (tempC * 9.0 / 5.0) + 32.0
+    humidity = dht_sensor.humidity()
+except Exception as e:
+    oled.text("Error DHT22", 0, 24)
+    oled.text(str(e), 0, 36)
+    oled.show()
+    tempC = tempF = humidity = soundSpeed = 0.0  # Reset to default
+    errorstate = True
+
+'''
+Todo; think about ultrasonic errors and fix this
+try:
+    ultra(soundSpeed, timeout=30000)
+except Exception as e:
+    oled.text("Error Ultrasonic", 0, 24)
+    #oled.text(str(e), 0, 36)
+    oled.show()
+    errorstate = True
+'''
 
 '''
 with canvas(device) as draw:
@@ -200,15 +218,16 @@ while True:
             print("Error reading DHT22:", str(e))
         
         # black dispay, then update results
-        oled.fill(0)
-        oled.text(f"TempF= {tempF:.1f}F", 0, 0)
-        oled.text(f"Humid= {humidity:.1f}%", 0, 12)
-        oled.text(f"Sound={soundSpeed:.1f}m/s", 0, 24)
-        oled.text(f"Dist= {dist:.1f}cm", 0, 55)
-        bitmap_artcar_image=bytearray(b'\xc9\x04\x59\x11\x0c\x08\x43\xc8\x82\x8e\x90\x93\x10\x93\xe0\x63\x00\x78\xe0\xe3\x07\xff\x9f\xff\xff\xff\xfc\xff\xc0\x00\x00\x00\x00\x00\x03\x40\x1c\xf3\xe3\x8e\x78\x02\x47\x22\x88\x84\x51\x44\xe2\x45\x22\x88\x84\x11\x44\xa2\x47\x22\xf0\x84\x11\x78\xe2\x20\x3e\x88\x84\x1f\x44\x04\x10\x22\x88\x84\x51\x44\x08\x0c\x22\x88\x83\x91\x44\x30\x03\x00\x00\x00\x00\x00\xc0\x00\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00')
-        fb = framebuf.FrameBuffer(bitmap_artcar_image,56,15, framebuf.MONO_HLSB)
-        oled.blit(fb,22,36)
-        oled.show()
+        if not errorstate:
+            oled.fill(0)
+            oled.text(f"TempF= {tempF:.1f}F", 0, 0)
+            oled.text(f"Humid= {humidity:.1f}%", 0, 12)
+            oled.text(f"Sound={soundSpeed:.1f}m/s", 0, 24)
+            oled.text(f"Dist= {dist:.1f}cm", 0, 55)
+            bitmap_artcar_image=bytearray(b'\xc9\x04\x59\x11\x0c\x08\x43\xc8\x82\x8e\x90\x93\x10\x93\xe0\x63\x00\x78\xe0\xe3\x07\xff\x9f\xff\xff\xff\xfc\xff\xc0\x00\x00\x00\x00\x00\x03\x40\x1c\xf3\xe3\x8e\x78\x02\x47\x22\x88\x84\x51\x44\xe2\x45\x22\x88\x84\x11\x44\xa2\x47\x22\xf0\x84\x11\x78\xe2\x20\x3e\x88\x84\x1f\x44\x04\x10\x22\x88\x84\x51\x44\x08\x0c\x22\x88\x83\x91\x44\x30\x03\x00\x00\x00\x00\x00\xc0\x00\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00')
+            fb = framebuf.FrameBuffer(bitmap_artcar_image,56,15, framebuf.MONO_HLSB)
+            oled.blit(fb,22,36)
+            oled.show()
     #
     #Every loop do this
     led.toggle()
