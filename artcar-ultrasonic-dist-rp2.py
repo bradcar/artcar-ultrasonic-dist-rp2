2e1e89b82a8b8353aa32fe5bc7683a2832406226
bradcar <bc@bradcarlile.com>
2024-10-15 18:41:28 -0700

    Update artcar-ultrasonic-dist-rp2.py
    working ic2 OLED 1309, displays sound speed, temp, humidity

--- artcar-ultrasonic-dist-rp2.py
+++ artcar-ultrasonic-dist-rp2.py
@@ -3,9 +3,40 @@ import machine
 import dht
 import time
 import math
+#ic2
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+'''
+from luma.core.interface.serial import spi
+from luma.oled.device import ssd1309
+from luma.core.render import canvas
+from PIL import ImageFont
+'''
 
+# pins
 dht_pin = machine.Pin(2)
 led = Pin(25, Pin.OUT)
+
+# ic2 pins
+# https://www.tomshardware.com/how-to/oled-display-raspberry-pi-pico
+
+# spi pins
+# vcc = 3.3v
+# gnd
+# scl (SCLK) GPIO 11 p15
+# SDA (MOSI) GPIO 10 p14
+# RES (RST)  GPIO 25? RESET p30
+# DC GPIO 24? GPIO 9 p12
+# CS chip select CE chip enable GPIO 8 p11
+
+'''
+#display SSD spi
+#serial = spi(device=0, port=0, gpio_SCLK=15, gpio_SDA=14, gpio_RST=30, gpio_DC=12, gpio_CE=11)
+serial = spi(port=0, device=0, gpio_DC=12, gpio_RST=25, gpio_CS=11)
+device = ssd1309(serial)
+'''
+i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
 
 timecall = Timer()
 
@@ -25,6 +56,13 @@ debug = True
 # startup code
 print("Starting...")
 print(f"Speed Sound: {soundSpeed:.1f} m/s")
+print()
+
+'''
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    draw.text((30, 40), "Hello World", fill="white")
+'''
 
 
 # main loop
@@ -41,7 +79,8 @@ while True:
         humidity = dht_sensor.humidity()
         # Speed sound with temp, best I could find was: (20.05 * sqrt(273.16 + tempC))
         # Speed sound temp & humidity calculator: http://resource.npl.co.uk/acoustics/techguides/speedair/
-        # I did a 2d linear fit with formula above to add correction, error is +/-0.07% in range
+        # sing online calculated constructed spreadsheet of values
+        # hten 2d linear fit with formula above to add correction, error is +/-0.07% in range
         # valid over the temp range 0 to 30C temp & 75 to 102 kPa pressure
         soundSpeed = (20.05 * math.sqrt(273.16 + tempC)) + (0.0006545 * humidity + 0.00475) * tempC + (0.001057 * humidity + 0.07121)
         #
@@ -50,6 +89,12 @@ while True:
             print(f"Temp °F : {tempF:.2f}")
             print(f"Humidity: {humidity:.2f}%")
             print(f"Speed Sound: {soundSpeed:.1f} m/s")
+        # fill with black and the reprint
+        oled.fill(0)
+        oled.text(f"Sound={soundSpeed:.1f}m/s", 0, 0)
+        oled.text(f"TempF= {tempF:.1f}F", 0, 12)
+        oled.text(f"humid= {humidity:.1f}%", 0, 24)
+        oled.show()
     except Exception as e:
         print("Error reading DHT22:", str(e))
     #
