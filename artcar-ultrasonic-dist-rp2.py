705b72146178ce4346eca59e550d3732ee6f265a
bradcar <bc@bradcarlile.com>
2024-10-20 21:44:11 -0700

    major python cleanup

--- artcar-ultrasonic-dist-rp2.py
+++ artcar-ultrasonic-dist-rp2.py
@@ -1,6 +1,8 @@
 # Raspberry Pi Pico 2 project - ArtCar ultrasonic - Oct 2024
 #
-# BUT right now ONLY HAVE 1 ULTRASONIC TESTED, oled SPI display: temp,humid,speedsound, & dist
+# Uses 3 ultrasonic sensors, uses oled SPI display to show measured
+# temp & humid and uses them to calculate speeed of sound for accurate
+# distance measurement.
 #
 # based on Brad's Arduino UNO and 128x64 OLED Display for rear&front parking sensor
 #    - speed of sound with temp & humidity correction
@@ -15,7 +17,7 @@
 #    Arduino GitHub:      https://github.com/bradcar/art-car-ultrasonic-dist
 #
 # project Based on GREAT work by upir!!!
-#     youtube full video: https://youtu.be/gg08H-6Z1Lo
+#     youTube full video: https://youtu.be/gg08H-6Z1Lo
 #     created by upir, 2022
 #
 # https://micropython.org/download/RPI_PICO2/ for latest .uf2 preview
@@ -25,7 +27,7 @@
 #  * try a02yyuw waterproof UART sensor, test code in /sensor-tests
 #  * add button #3 for switching between showing car on oled or
 
-from machine import Pin, Timer
+from machine import Pin
 import machine
 import dht
 import time
@@ -44,7 +46,7 @@ from framebuf import FrameBuffer, MONO_HLSB
 # Constants and setup
 DISP_WIDTH=128
 DISP_HEIGHT=64
-# Define the number of sensors, more than 3 is redesign
+# Define the number of sensors, more than 3 will need redesign
 NUMBER_OF_SENSORS = 3
 
 class SensorData:
@@ -222,14 +224,6 @@ bitmap_sensor_1a_off = bytearray([
   0x05,0x55,0x55,0x40,0x0a,0xaa,0xaa,0xa0,0x05,0x55,0x55,0x40,0x0a,0xaa,0xaa,0xa0,
   0x15,0x55,0x55,0x50
 ])
-# 'sensor_0d_on', 32x18px
-bitmap_sensor_0d_on = bytearray([
-  0x20,0x00,0x00,0x00,0x38,0x00,0x00,0x00,0x7f,0x00,0x00,0x00,0x7f,0xc0,0x00,0x00,
-  0xff,0xf8,0x00,0x00,0xff,0xff,0x00,0x00,0xff,0xff,0xf0,0x00,0xff,0xff,0xfe,0x00,
-  0x7f,0xff,0xff,0xf0,0x3f,0xff,0xff,0xfe,0x0f,0xff,0xff,0xfe,0x03,0xff,0xff,0xfe,
-  0x00,0x7f,0xff,0xfe,0x00,0x1f,0xff,0xfe,0x00,0x01,0xff,0xfc,0x00,0x00,0x3f,0xfc,
-  0x00,0x00,0x03,0xfc,0x00,0x00,0x00,0x1c
-])
 # 'sensor_1a_on', 32x9px
 bitmap_sensor_1a_on = bytearray([
   0x07,0xff,0xff,0xc0,0x0f,0xff,0xff,0xe0,0x0f,0xff,0xff,0xe0,0x0f,0xff,0xff,0xe0,
@@ -386,20 +380,17 @@ debug = False
 
 # Button debouncer with efficient interrupts, which don't take CPU cycles!
 # https://electrocredible.com/raspberry-pi-pico-external-interrupts-button-micropython/
-def callback_1(pin):
-    global interrupt_1_flag, debounce_1_time
-    if (time.ticks_ms() - debounce_1_time) > 500:
-        interrupt_1_flag= 1
-        debounce_1_time=time.ticks_ms()
-        
-def callback_2(pin):
-    global interrupt_2_flag, debounce_2_time
-    if (time.ticks_ms() - debounce_2_time) > 500:
-        interrupt_2_flag= 1
-        debounce_2_time=time.ticks_ms()
+def callback(pin):
+    global interrupt_1_flag, interrupt_2_flag, debounce_1_time, debounce_2_time
+    if pin == button_1 and (int(time.ticks_ms()) - debounce_1_time) > 500:
+        interrupt_1_flag = 1
+        debounce_1_time = time.ticks_ms()
+    elif pin == button_2 and (int(time.ticks_ms()) - debounce_2_time) > 500:
+        interrupt_2_flag = 1
+        debounce_2_time = time.ticks_ms()
 
-button_1.irq(trigger=Pin.IRQ_FALLING, handler=callback_1)
-button_2.irq(trigger=Pin.IRQ_FALLING, handler=callback_2)
+button_1.irq(trigger=Pin.IRQ_FALLING, handler=callback)
+button_2.irq(trigger=Pin.IRQ_FALLING, handler=callback)
 
 def initialize_flipped_bitmaps():
     """
@@ -459,17 +450,11 @@ def get_str_width(text):
     # MicroPython has 8x8 fonts  x*9-1 ?
     return len(text) 
     
-def calc_speed_sound(speed_sound, temp_c, temp_f, humidity):
+def calc_speed_sound():
     """
-    calc speed of sound based on temp & humidity from the DHT22 sensor
-    
-    param:speed_sound (float): speed of sound in cm.
-    param:speed of sound (float): speed of sound in cm.
-    param:temp_c (float): temparature in C
-    param:temp_f (float): temparature in F
-    param:humidity (float): humidity %  
+    calc to update speed of sound based on temp & humidity from the DHT22 sensor
     """
-    global dht_error, debug
+    global dht_error, speed_sound, temp_c, temp_f, humidity
     
     try:
         dht_sensor.measure()
@@ -494,7 +479,7 @@ def calc_speed_sound(speed_sound, temp_c, temp_f, humidity):
             print(f"Speed Sound: {speed_sound:.1f} m/s\n")
     except Exception as e:
         print("Error reading DHT22:", str(e))
-    return(speed_sound, temp_c, temp_f, humidity)
+    return
 
 def ultrasonic_distance_uart(i):
     """
@@ -524,25 +509,24 @@ def ultrasonic_distance_uart(i):
     * need RS-485 twisted pair Data Serial Bulk Cable: 24 AWG
     * PH2.0-4P Connector x1 used by A02YYUW
     
-    :param i (int): index for ultrasonic sensor.
+    :param i: index for ultrasonic sensor.
     :returns: cm distance or an error value.
     """
     # https://forum.makerforums.info/t/a02yyuw-waterproof-ultrasonic-sensor/87359/18
-    print("ultrasonic_distance_uart - NOT IMPLEMENTED")
+    print(f"ultrasonic_distance_uart #{i} - NO code IMPLEMENTED")
     return
     
-def ultrasonic_distance_pwm(i, speed, timeout=50000):
+def ultrasonic_distance_pwm(i, timeout=50000):
     """
-    Get ultrasonic distance using an  sensor (you send signal & time) with a timeout.
+    Get ultrasonic distance from a sensor where ping and measure with a timeout.
     
     HC-SR04: most Send a 10uS high to trigger
     JSN-SR04T: Send a 20uS high to trigger, instead of 10
     A02YYUW: only outputs UART serial data, instead use ultrasonic_distance_uart(i) 
     https://dronebotworkshop.com/waterproof-ultrasonic/
     
-    :param i (int): index for ultrasonic sensor.
-    :param speed (float): Speed of sound in m/s.
-    :param timeout (int): Max usecs to wait for the echo signal return.
+    :param i: index for ultrasonic sensor.
+    :param timeout: Max usecs to wait for the echo signal return. default 50ms.
     :returns: cm distance or an error value.
     """
     trigger[i].low()
@@ -570,25 +554,17 @@ def ultrasonic_distance_pwm(i, speed, timeout=50000):
     signal_on = utime.ticks_us()
 
     # Calculate cm distance
-    return (signal_on - signal_off) * (speed / 20000.0)
+    return utime.ticks_diff(signal_on, signal_off) * (speed_sound / 20000.0)
 
-def display_environment(temp_c, temp_f, humidity, speed_sound, dist):
+def display_environment(dist):
     """
-    display just environment readings & car image for fun,
+    display just environment readings & car image(for fun)
     No need to oled.fill(0) before or oled.show() after call
 
-    param:speed of sound (float): speed of sound in cm.
-    param:temp_c (float): temparature in C
-    param:temp_f (float): temparature in F
-    param:humidity (float): humidity %
-    param:dist (float):  distance if dist = -1.0 then display error
-    param:speed_sound (float): speed of sound in cm.
-    """
-    global metric, working_sensors, dht_error, debug
-    
+    param:dist:  distance if dist = -1.0 then display error
+    """    
     oled.fill(0)
     if not dht_error:
-        temp_f = (temp_c * 9.0 / 5.0) + 32.0
         oled.fill(0)
         if metric:
             oled.text(f"Temp = {temp_c:.1f}C", 0, 0)
@@ -642,15 +618,14 @@ def flip_bitmap_vert(bitmap, width, height):
     return flipped_bitmap
 
 
-def blit_white_only(oled, source_fb, w, h, x, y):
+def blit_white_only(source_fb, w, h, x, y):
     """
     only send white pixels one by one
-    :param oled: display
-    :param source_fb:
+    :param source_fb: framebugger to flip
     :param w: width of the framebuffer
     :param h: height of the framebuffer
-    :x: x-position for display
-    :x: y-position for display
+    :param x: x-position for display
+    :param y: y-position for display
     """
     for row in range(h):
         for col in range(w):
@@ -659,15 +634,10 @@ def blit_white_only(oled, source_fb, w, h, x, y):
             if pixel == 1:  # Only copy white pixels
                 oled.pixel(x + col, y + row, 1)  # Set the pixel on the OLED
                 
-def display_car (temp_c, temp_f):
+def display_car ():
     """
     display_car & temp, Need oled.fill(0) before call & oled.show() after call
-
-    param:temp_c (float): temparature in C
-    param:temp_f (float): temparature in F
-    """
-    global rear, metric, dht_error, debug
-    
+    """    
     if rear:
         oled.blit(FrameBuffer(bitmap_artcar_image_back,56,15, MONO_HLSB), 36, 0)
         oled.blit(FrameBuffer(degree_temp,24,10, MONO_HLSB), 104, 0)
@@ -706,25 +676,10 @@ def display_tiles_dist():
     """
     display_car & temp, Need oled.show() after call
     """
-    global sensor, working_sensors, rear, dht_error, debug, dist_step_01, dist_step_02, dist_step_03, dist_step_04
-    global flipped_bitmap_sensor_0a_off, flipped_bitmap_sensor_0a_on
-    global flipped_bitmap_sensor_0b_off, flipped_bitmap_sensor_0b_on
-    global flipped_bitmap_sensor_0c_off, flipped_bitmap_sensor_0c_on
-    global flipped_bitmap_sensor_0d_off, flipped_bitmap_sensor_0d_on
-    global flipped_bitmap_sensor_1a_off, flipped_bitmap_sensor_1a_on
-    global flipped_bitmap_sensor_1b_off, flipped_bitmap_sensor_1b_on
-    global flipped_bitmap_sensor_1c_off, flipped_bitmap_sensor_1c_on
-    global flipped_bitmap_sensor_1d_off, flipped_bitmap_sensor_1d_on
-    global flipped_bitmap_sensor_2a_off, flipped_bitmap_sensor_2a_on
-    global flipped_bitmap_sensor_2b_off, flipped_bitmap_sensor_2b_on
-    global flipped_bitmap_sensor_2c_off, flipped_bitmap_sensor_2c_on
-    global flipped_bitmap_sensor_2d_off, flipped_bitmap_sensor_2d_on
-
     if not working_sensors:
         oled.text(" No Ultrasonic",  5, 30)
         oled.text("Sensors Working", 5, 40)
         return
-
 
     if rear:
         #Middle
@@ -766,10 +721,10 @@ def display_tiles_dist():
                 bmp_1d = FrameBuffer(bitmap_sensor_0d_on, 32, 18, MONO_HLSB)  #, 16, 43)
             else:
                 bmp_1d = FrameBuffer(bitmap_sensor_0d_off, 32, 18, MONO_HLSB)  #, 16, 43)
-            blit_white_only(oled, bmp_1a, 32, 14, 24, 17)
-            blit_white_only(oled, bmp_1b, 32, 16, 21, 25)
-            blit_white_only(oled, bmp_1c, 32, 17, 18, 34)
-            blit_white_only(oled, bmp_1d, 32, 18, 16, 43)
+            blit_white_only(bmp_1a, 32, 14, 24, 17)
+            blit_white_only(bmp_1b, 32, 16, 21, 25)
+            blit_white_only(bmp_1c, 32, 17, 18, 34)
+            blit_white_only(bmp_1d, 32, 18, 16, 43)
 
         # right sensor
         if 2 in working_sensors:
@@ -790,10 +745,10 @@ def display_tiles_dist():
                 bmp_3d = FrameBuffer(bitmap_sensor_2d_on, 32, 18, MONO_HLSB)  #, 80, 43)
             else:
                 bmp_3d = FrameBuffer(bitmap_sensor_2d_off, 32, 18, MONO_HLSB)  #, 80, 43)
-            blit_white_only(oled, bmp_3a, 32, 14, 72, 17)
-            blit_white_only(oled, bmp_3b, 32, 16, 74, 25)
-            blit_white_only(oled, bmp_3c, 32, 17, 77, 34)
-            blit_white_only(oled, bmp_3d, 32, 18, 80, 43)
+            blit_white_only(bmp_3a, 32, 14, 72, 17)
+            blit_white_only(bmp_3b, 32, 16, 74, 25)
+            blit_white_only(bmp_3c, 32, 17, 77, 34)
+            blit_white_only(bmp_3d, 32, 18, 80, 43)
         
     # This is for flipped displays
     else:
@@ -836,10 +791,10 @@ def display_tiles_dist():
                 bmp_1d = FrameBuffer(flipped_bitmap_sensor_0d_on, 32, 18, MONO_HLSB)
             else:
                 bmp_1d = FrameBuffer(flipped_bitmap_sensor_0d_off, 32, 18, MONO_HLSB)
-            blit_white_only(oled, bmp_1a, 32, 14, 72, DISP_HEIGHT-17-14)
-            blit_white_only(oled, bmp_1b, 32, 16, 74, DISP_HEIGHT-25-16)
-            blit_white_only(oled, bmp_1c, 32, 17, 77, DISP_HEIGHT-34-17)
-            blit_white_only(oled, bmp_1d, 32, 18, 80, DISP_HEIGHT-43-18)
+            blit_white_only(bmp_1a, 32, 14, 72, DISP_HEIGHT-17-14)
+            blit_white_only(bmp_1b, 32, 16, 74, DISP_HEIGHT-25-16)
+            blit_white_only(bmp_1c, 32, 17, 77, DISP_HEIGHT-34-17)
+            blit_white_only(bmp_1d, 32, 18, 80, DISP_HEIGHT-43-18)
 
         # right sensor
         if 2 in working_sensors:
@@ -860,10 +815,10 @@ def display_tiles_dist():
                 bmp_3d = FrameBuffer(flipped_bitmap_sensor_2d_on, 32, 18, MONO_HLSB)
             else:
                 bmp_3d = FrameBuffer(flipped_bitmap_sensor_2d_off, 32, 18, MONO_HLSB)
-            blit_white_only(oled, bmp_3a, 32, 14, 24, DISP_HEIGHT-17-14)
-            blit_white_only(oled, bmp_3b, 32, 16, 21, DISP_HEIGHT-25-16)
-            blit_white_only(oled, bmp_3c, 32, 17, 18, DISP_HEIGHT-34-17)
-            blit_white_only(oled, bmp_3d, 32, 18, 16, DISP_HEIGHT-43-18)
+            blit_white_only(bmp_3a, 32, 14, 24, DISP_HEIGHT-17-14)
+            blit_white_only(bmp_3b, 32, 16, 21, DISP_HEIGHT-25-16)
+            blit_white_only(bmp_3c, 32, 17, 18, DISP_HEIGHT-34-17)
+            blit_white_only(bmp_3d, 32, 18, 16, DISP_HEIGHT-43-18)
 
     #round to nearest
     for i in working_sensors:
@@ -907,7 +862,7 @@ temp_f = 69.0
 humidity = 70.0
 speed_sound = SPEED_SOUND_20C_70H
 oled.fill(0)
-display_car(temp_c, temp_f)
+display_car()
 oled.show()
 zzz(3)
 
@@ -926,7 +881,7 @@ except Exception as e:
 working_sensors = []
 nonworking_sensors = []
 for i in range(NUMBER_OF_SENSORS):
-    distance = ultrasonic_distance_pwm(i, speed_sound)
+    distance = ultrasonic_distance_pwm(i, timeout=50000)
     if 0.0 < distance < 300.0:
         working_sensors.append(i)
     else:
@@ -960,26 +915,26 @@ while True:
     # ...humidity effect is negligible, but I had a dht22 which does both, so why not :)
     if elapsed_time > 3000:
         loop_time = time.ticks_ms()
-        speed_sound, temp_c, temp_f, humidity = calc_speed_sound(speed_sound, temp_c, temp_f, humidity)
+        calc_speed_sound()
     
     # left front/rear = 0, middle=1  right front/rear =2
     for i in working_sensors:
         #get distance from ultrasonic sensor, 30ms round trip 514cm or 202in
-        sensor[i].cm = ultrasonic_distance_pwm(i, speed_sound, timeout=30000)
+        sensor[i].cm = ultrasonic_distance_pwm(i, timeout=30000)
         sensor[i].inch = sensor[i].cm/2.54
     
     if show_env:
         if working_sensors:
-            display_environment(temp_c, temp_f, humidity, speed_sound, sensor[working_sensors[0]].cm)
+            display_environment(sensor[working_sensors[0]].cm)
         else:
-            display_environment(temp_c, temp_f, humidity, speed_sound, -1.0)
+            display_environment(-1.0)
     else:
         oled.fill(0)
-        display_car(temp_c, temp_f)
+        display_car()
         display_tiles_dist()
         oled.show()
 
-    #Every loop do this
+    # Every loop do this
     led.toggle()
     time.sleep_ms(300)
     
