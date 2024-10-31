3c6d6ed015ddafc83fc8c0f5e383110b8efe1d7a
bradcar <bc@bradcarlile.com>
2024-10-31 12:54:20 -0700

    updated dht22 code that also include commented out functions that can be use in other codes

--- sensor-tests/dht22.py
+++ sensor-tests/dht22.py
@@ -20,6 +20,48 @@ led = Pin(25, Pin.OUT)
 
 dht_sensor = dht.DHT22(dht_pin)
 
+# functions that can be used in other codes
+# #
+# def calc_speed_sound(celsius, percent_humidity):
+#     """
+#     calc to update speed of sound based on temp & humidity from the DHT22 sensor
+# 
+#     :param celsius: temp in Celsius
+#     :param percent_humidity: % humidity
+#     :returns: speed meter/sec, error string
+#     """
+#     if celsius > 0 and percent_humidity > 0:
+#         # Speed sound with temp correction: (20.05 * sqrt(273.16 + temp_c))
+#         # online temp/humid calc:
+#         #   http://resource.npl.co.uk/acoustics/techguides/speedair/
+#         # created spreadsheet of diffs between above temp formula and online temp/humid
+#         # did a 2d linear fit to create my own correction, error is now +/-0.07%
+#         # valid for 0C to 30C temp & 75 to 102 kPa pressure
+#         meter_per_sec = (20.05 * sqrt(273.16 + celsius)) \
+#                         + (0.0006545 * percent_humidity + 0.00475) * celsius \
+#                         + (0.001057 * percent_humidity + 0.07121)
+#         return meter_per_sec, None
+#     else:
+#         return None, f"ERROR_INVALID_SOUND_SPEED:temp={celsius}C,humidity={percent_humidity}%"
+# #    
+# def dht_temp_humidity():
+#     """
+#     read temp & humidity from the DHT22 sensor, measurement takes ~271ms
+# 
+#     :returns: celsius, percent_humidity, error string
+#     """
+#     try:
+#         dht_sensor.measure()
+#         celsius = dht_sensor.temperature()
+#         percent_humidity = dht_sensor.humidity()
+#         if debug:
+#             print(f"DHT Temp °C : {temp_c:.2f}")
+#             print(f"DHT Temp °F : {temp_f:.2f}")
+#             print(f"DHT Humidity: {humidity:.2f}%")
+#     except Exception as e:
+#         return None, None, "ERROR_TEMP_HUMID:" + str(e)
+#     return celsius, percent_humidity, None
+
 # startup code
 print("Starting...")
 print("====================================")
