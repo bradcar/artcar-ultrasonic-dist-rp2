ee5a968872b1bfc67474b43ad3cac46a1b1b01ea
bradcar <bc@bradcarlile.com>
2024-11-03 18:09:25 -0800

    updated font testor to show all fonts, most of Hinch's are variable width

--- sensor-tests/font-tester.py
+++ sensor-tests/font-tester.py
@@ -2,13 +2,16 @@
 #
 # Font tester based on Peter Hinch's great work
 # notice need short_writer.py to make it work
-
-# short_writer Code created by: Charlotte Swift  - THANKS!!
+#
+# short_writer.py  Code created by Peter Hinch modified by Charlotte Swift  - THANKS!!
 # https://www.youtube.com/watch?v=kxlN1knBpQ0
 # modified to fix a few errors
+#
 # saved as writer_short.py in /lib on Raspberry Pi
 #
-# adaptation bradcar
+# OLED library: https://docs.micropython.org/en/latest/esp8266/tutorial/ssd1306.html
+#
+# by bradcar
 
 from machine import Pin
 import machine
@@ -22,11 +25,18 @@ from time import sleep as zzz
 from writer_short import Writer
 
 # Peter Hinch fonts: https://github.com/peterhinch/micropython-font-to-py
-# other fonts: https://github.com/easytarget/microPyEZfonts/tree/main/examples/fonts
-import freesans14 as font0
-import freesans17 as font1
-import freesans20 as font2
-import ez5x7 as font3
+#     other fonts: https://github.com/easytarget/microPyEZfonts/tree/main/examples/fonts
+
+# Fixed width fonts
+#    defualt micropython font #  7px high 8px wide
+#    default font fixed width
+import ez5x7 as font_5px         #  5px high 7px wide, needs short_writer.py
+
+# Variable width fonts, all need short_writer.py
+import freesans14 as font_14px   # 14px high
+import freesans17 as font_17px   # 17px high
+import freesans20 as font_20px   # 20px high
+
 #ic2
 #from machine import I2C
 #from ssd1306 import SSD1306_I2C
@@ -55,10 +65,10 @@ oled_spi = machine.SPI(1)
 print("oled_spi:", oled_spi)
 oled = SSD1306_SPI(128, 64, oled_spi, dc, res, cs)
 
-#  DISPLAY IMAGES
+# DISPLAY IMAGES
 # image2cpp (convert png into C code): https://javl.github.io/image2cpp/
 # then replace ", 0"  with "\"
-# const unsigned char bitmap_artcar_image[] PROGMEM = {0xc9, 0x04, 0x59, ...
+# const unsigned char_index bitmap_artcar_image[] PROGMEM = {0xc9, 0x04, 0x59, ...
 # goes to bitmap_artcar_image=bytearray(b'\xc9\x04\x59
 
 # 'art-car-imag', 56x15px
@@ -68,36 +78,57 @@ bitmap_artcar_image=bytearray(b'\xc9\x04\x59\x11\x0c\x08\x43\xc8\x82\x8e\x90\x93
 # fonts: https://www.youtube.com/watch?v=kxlN1knBpQ0
 
 # startup code
-# startup code
 print("Starting...")
 print("====================================")
 print(implementation[0], uname()[3],
-      "\nrun on", uname()[4])
+      "\nrun on:", uname()[4])
 print("====================================")
 
-# at start display artcar image at top of oled
+# at start display artcar image at bottom of oled
 oled.fill(0)
-fb = framebuf.FrameBuffer(bitmap_artcar_image,56,15, framebuf.MONO_HLSB)
-oled.blit(fb,40,8)
-oled.text("1234567890", 26, 24)
+# fb = framebuf.FrameBuffer(bitmap_artcar_image,56,15, framebuf.MONO_HLSB)
+# oled.blit(fb,40,128-15)
 
-font_writer = Writer(oled,font3, verbose=False)
-font_writer_2 = Writer(oled,font2, verbose=False)
+text_5px = Writer(oled,font_5px, verbose=False)
+text_14px = Writer(oled,font_14px, verbose=False)
+text_17px = Writer(oled,font_17px, verbose=False)
+text_20px = Writer(oled,font_20px, verbose=False)
 
+offset = 0
 while True:
-    for char in range (32, 126):
-        font_writer.set_textpos(0,32)
-        font_writer.printstring(f"{char}: {chr(char)} ")
-        print(f"{char}")
+    for char_index in range (32, 126):
+        oled.fill(0)
         
-        font_writer_2.set_textpos(0,38)
-        font_writer_2.printstring(f"{char}: {chr(char)} ")
-        print(f"{char}")
+        oled.text("1234567890", 49, 0)
+        
+        print(f"{char_index}")
+        offset = 3
+        
+        text_5px.set_textpos(26, offset)
+        text_5px.printstring(f"{char_index}: {chr(char_index)}")
+        
+        offset = offset+5+1
+        oled.hline(50, offset, 20, 1)
+        text_14px.set_textpos(11, offset)
+        text_14px.printstring(f"{char_index}: {chr(char_index)}")
+   
+        offset = offset+14+1
+        oled.hline(50, offset, 20, 1)
+        text_17px.set_textpos(8, offset)
+        text_17px.printstring(f"{char_index}: {chr(char_index)}")
+
+        offset = offset+17+1
+        oled.hline(50, offset, 20, 1)
+        text_20px.set_textpos(0, offset)
+        text_20px.printstring(f"{char_index}: {chr(char_index)}")
+        oled.hline(50, offset, 20, 1)
+        
+        offset = offset+20+1
+        oled.hline(50, offset, 20, 1)
+        print(f"{offset=}")
         
         zzz(.3)
         oled.show()
-
-# main loop
 
     led.toggle()
     time.sleep_ms(500)
